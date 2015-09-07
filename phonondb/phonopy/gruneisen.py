d39ea678099cf87850c8ebe66bcc732012138a9b
Atsushi Togo <atz.togo@gmail.com>
2015-09-07 15:24:06 +0200

    Update Gruneisen script for making figure

--- phonondb/phonopy/gruneisen.py
+++ phonondb/phonopy/gruneisen.py
@@ -3,15 +3,12 @@ from cogue.crystal.utility import klength2mesh
 
 class ModeGruneisen:
     def __init__(self,
-                 phonon_orig,
-                 phonon_plus,
-                 phonon_minus,
+                 phonopy_gruneisen,
                  distance=100):
         self._phonopy_gruneisen = phonopy_gruneisen
-        self._phonon = phonon
-        self._lattice = np.array(phonon.get_unitcell().get_cell().T,
+        self._phonon = self._phonopy_gruneisen.get_phonon()
+        self._lattice = np.array(self._phonon.get_unitcell().get_cell().T,
                                  dtype='double')
-
         self._mesh = None
         self._gruneisen = None
 
@@ -20,7 +17,8 @@ class ModeGruneisen:
         if self._run_mesh_sampling():
             self._run_gruneisen()
             return True
-        return False
+        else:
+            return False
 
     def get_lattice(self):
         return self._lattice
@@ -29,28 +27,25 @@ class ModeGruneisen:
         return self._mesh
 
     def get_mesh_gruneisen(self):
-        return self._gruneisen
+        return self._gruneisen_mesh
 
-    def plot(self, plt, max_index=101):
-        temps, fe, entropy, cv = self._thermal_properties
+    def plot(self, 
+             plt,
+             cutoff_frequency=0.1):
         fig = plt.figure()
-        fig.subplots_adjust(left=0.20, right=0.92, bottom=0.18)
+        fig.subplots_adjust(left=0.20, right=0.93, bottom=0.13, top=0.95)
         plt.tick_params(axis='both', which='major', labelsize=10.5)
         ax = fig.add_subplot(111)
-        plt.plot(temps[:max_index], fe[:max_index], 'r-')
-        plt.plot(temps[:max_index], entropy[:max_index], 'b-')
-        plt.plot(temps[:max_index], cv[:max_index], 'g-')
+        for f, g in zip(self._frequencies.T, self._gammas.T):
+            condition = np.abs(f) > cutoff_frequency
+            plt.plot(np.extract(condition, f), np.extract(condition, g), 
+                     'o', color=(0, 0, 0), markersize=1.0)
         xlim = ax.get_xlim()
         ylim = ax.get_ylim()
         aspect = (xlim[1] - xlim[0]) / (ylim[1] - ylim[0])
-        # ax.set_aspect(aspect * 0.7)
-        plt.legend(('Free energy [kJ/mol]', 'Entropy [J/K/mol]',
-                    r'C$_\mathrm{V}$ [J/K/mol]'),
-                   loc='best',
-                   prop={'size':8.5},
-                   frameon=False)
-        plt.xlabel("Temperatures (K)")
-        plt.ylabel("Thermal properties (*/unitcell)")
+        ax.set_aspect(aspect * 1.0)
+        plt.xlabel("Frequency (THz)")
+        plt.ylabel("Mode Gruneisen parameter")
         
     def save_figure(self, plt):
         plt.savefig("gruneisen.png")
@@ -62,13 +57,14 @@ class ModeGruneisen:
         return self._phonopy_gruneisen.set_mesh(self._mesh)
     
     def _run_gruneisen(self):
-        self._thermal_properties = self._phonon.get_thermal_properties()
-    
+        self._gruneisen_mesh = self._phonopy_gruneisen.get_mesh()
+        self._gammas = self._gruneisen_mesh.get_gruneisen()
+        self._frequencies = self._gruneisen_mesh.get_frequencies()
 
 if __name__ == '__main__':
     import sys
     import yaml
-    from phonopy import Phonopy
+    from phonopy import Phonopy, PhonopyGruneisen
     from phonopy.gruneisen.mesh import Mesh as GruneisenMesh
     from phonopy.interface.phonopy_yaml import phonopyYaml
     from phonopy.file_IO import parse_FORCE_SETS
@@ -76,26 +72,25 @@ if __name__ == '__main__':
     import matplotlib
 
     matplotlib.use('Agg')            
-    matplotlib.rcParams.update({'figure.figsize': (4.5, 3)})
+    matplotlib.rcParams.update({'figure.figsize': (4, 4)})
     import matplotlib.pyplot as plt
 
     phonons = []
     for dirname in ('orig', 'plus', 'minus'):
-        if len(sys.argv) > 1:
-            cell = phonopyYaml("%s/" % dirname + sys.argv[1]).get_atoms()
-        else:
-            cell = phonopyYaml("%s/POSCAR-unitcell.yaml" % dirname).get_atoms()
+        cell = phonopyYaml("%s/POSCAR-unitcell.yaml" % dirname).get_atoms()
         phonon_info = yaml.load(open("%s/%s.yaml" % (dirname, dirname)))
         phonon = Phonopy(cell,
                          phonon_info['supercell_matrix'],
                          is_auto_displacements=False)
-        force_sets = parse_FORCE_SETS()
+        force_sets = parse_FORCE_SETS(filename="%s/FORCE_SETS" % dirname)
         phonon.set_displacement_dataset(force_sets)
         phonon.produce_force_constants()
         phonons.append(phonon)
 
-    distance = 100
-    gruneisen = ModeGruneisen(phonons[0], phonons[1], phonons[2], distance=distance)
+    phonopy_gruneisen = PhonopyGruneisen(phonons[0], phonons[1], phonons[2])
+    distance = 200
+    gruneisen = ModeGruneisen(phonopy_gruneisen,
+                              distance=distance)
     if gruneisen.run():
         gruneisen.plot(plt)
         lattice = gruneisen.get_lattice()
@@ -103,5 +98,6 @@ if __name__ == '__main__':
         print "alpha, beta, gamma =", get_angles(lattice)
         print "mesh (x=%f) =" % distance, gruneisen.get_mesh()
         gruneisen.save_figure(plt)
+        # plt.show()
     else:
         print "Mode Gruneisen parameter calculation failed."
