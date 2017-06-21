769e20a9b6c2bf45b9ae73dd73a741a3b8116ce0
Atsushi Togo <atz.togo@gmail.com>
2017-06-21 21:44:10 +0900

    Update shell scripts

--- shell/move_phonon.sh
+++ shell/move_phonon.sh
@@ -1,25 +1,22 @@
 #!/bin/zsh
 
-for i in `cat id_nums.dat`;do
+data_dir="../phonon-data/"
+www_dir="./"
+for i in `cat $data_dir/mp-list.dat|sed s/mp-//`;do
     d=`printf "%06d\n" $i|cut -c 1-3`
     echo $i $d
 
-    mv mp-$i.tar.lzma ../www/d$d
+    # mv mp-$i.tar.lzma ../www/d$d
 
-    cp -a mp-$i/gruneisen.png ../www/d$d/mp-$i-gruneisen.png
-    cp -a mp-$i/gruneisen-01/dos.png ../www/d$d/mp-$i-dos.png
-    if [ -f mp-$i/qha.png ]; then
-	cp -a mp-$i/qha.png ../www/d$d/mp-$i-qha.png
+    if [ -f $data_dir/mp-$i/dos.png ]; then
+      cp -a $data_dir/mp-$i/dos.png $www_dir/d$d/mp-$i-dos.png
     fi
 
-    if [ -f mp-$i/POSCAR-unitcell-mass.yaml ]; then
-    	cp -a mp-$i/gruneisen-01/POSCAR-unitcell-mass.yaml ../www/d$d/mp-$i-POSCAR.yaml
-    else
-    	cp -a mp-$i/gruneisen-01/POSCAR-unitcell.yaml ../www/d$d/mp-$i-POSCAR.yaml
+    if [ -f $data_dir/mp-$i/band.png ]; then
+      cp -a $data_dir/mp-$i/band.png $www_dir/d$d/mp-$i-band.png
     fi
 
-    ratio=`grep ratio mp-$i/gruneisen-01/imag_ratio.dat|awk '{print $2}'`
-    if [ `echo "$ratio < 0.01"|bc` -eq 1 ]; then
-	cp -a mp-$i/gruneisen-01/tprops.png ../www/d$d/mp-$i-tprops.png
+    if [ -f $data_dir/mp-$i/tprops.png ]; then
+      cp -a $data_dir/mp-$i/tprops.png $www_dir/d$d/mp-$i-tprops.png
     fi
 done
--- shell/run_sphinx.zsh
+++ shell/run_sphinx.zsh
@@ -1,11 +1,5 @@
 #!/bin/zsh
 
-cp ~/code/phonondb/sphinx/{index.rst,about.rst,database-mp.rst,conf.py,webserver.jpg} .
-
-for i in {000..999};do 
-    if [ `find d$i -name "*.lzma"|wc|awk '{print $1}'` -gt 0 ];then    
-	echo "   d$i/index" >> database-mp.rst
-    fi
-done
+#cp ~/code/phonondb/sphinx/{index.rst,about.rst,conf.py,webserver.jpg} .
 
 sphinx-build -b html . _build
